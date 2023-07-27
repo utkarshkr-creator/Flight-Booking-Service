385f303f0454ce5cbd78ea3d154236b54b7d02cd
Utkarshkr-creater <2016krutkarsh@gmail.com>
2023-07-27 22:52:47 +0530

    add transaction to cancel booking

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -50,7 +50,7 @@ async function makePayment(data) {
         const bookingTime = new Date(bookingDetails.createdAt);
         const currentTime = new Date();
         if(currentTime - bookingTime > 300000) {
-            await bookingRepository.update(data.bookingId, {status: CANCELLED}, transaction);
+            await cancelBooking(data.bookingId);
             throw new AppError('The booking has expired', StatusCodes.BAD_REQUEST);
         }
         if(bookingDetails.totalCost != data.totalCost) {
@@ -69,8 +69,30 @@ async function makePayment(data) {
     }
 }
 
+async function cancelBooking(bookingId){
+     const transaction=await db.sequelize.transaction();  
+     try {
+        const bookingDetails = await bookingRepository.get(bookingId, transaction);
+        if(bookingDetails.status == CANCELLED) {
+           await transaction.commit();  
+           return true;
+        }
+        await axios.patch(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${bookingDetails.flightId}/seats`, {
+            seats: bookingDetails.noofSeats,
+            dec:0,
+        });
+        await bookingRepository.update(bookingId, {status: CANCELLED}, transaction);
+        await transaction.commit();
+        return true;
+
+     } catch (error) {
+        await transaction.rollback();
+        throw error;
+     }
+}
 
 module.exports={
     createBooking,
-    makePayment
+    makePayment,
+    cancelBooking
 }
